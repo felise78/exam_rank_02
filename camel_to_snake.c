115b692178f1eca181de1b1320b72d934c778fc4
Helene MOTTU <hemottu@e1r5p8.clusters.42paris.fr>
2023-02-28 11:07:57 +0100

    camel_to_snake / ft_range / rev_print / rot_13

---
+++ camel_to_snake.c
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   camel_to_snake.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hemottu <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/27 20:34:03 by hemottu           #+#    #+#             */
+/*   Updated: 2023/02/27 20:49:50 by hemottu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+
+int ft_wordcount(char *str)
+{
+	int i = 0;
+	int j = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			j++;
+		i++;
+	}
+	return (j);
+}
+
+int ft_strlen(char *str)
+{
+	int i = 0;
+	while (str[i])
+		i++;
+	return(i);
+}
+
+void ft_print(char *str)
+{
+	int i = 0;
+	int len = ft_strlen(str) + ft_wordcount(str);
+	char *phrase;
+	phrase = malloc(sizeof(char) * len + 1);
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			write(1, "_", 1);
+			str[i] = str[i] + 32;
+		}
+		write(1, &str[i], 1);
+		i++;
+	}
+	free(phrase);
+}
+
+int main (int ac, char **av)
+{
+	if (ac == 2)
+	{
+		ft_print(av[1]);
+	}
+	write (1, "\n", 1);
+	return(0);
+}
---
+++ ft_range.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hemottu <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/27 20:51:07 by hemottu           #+#    #+#             */
+/*   Updated: 2023/02/27 21:12:05 by hemottu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int     *ft_range(int start, int end)
+{
+	int *tab;
+	int len;
+	int i = 0;
+
+	if (start == end)
+	{
+		tab = malloc(sizeof(int) * 1);
+		tab[0] = start;
+	}
+	else if (start > end)
+	{
+		len = start - end + 1;
+		tab = malloc(sizeof(int) * len);
+                 while (start >= end)
+                  {
+                          tab[i] = start;
+                          start--;
+                          i++;
+		  }
+ 	}
+	else if (end > start)
+	{
+		len = end - start + 1;
+		tab = malloc(sizeof(int) * len);
+		while (start <= end)
+		{
+			tab[i] = start;
+			start++;
+			i++;
+		}	
+	}
+	return(tab);
+}
+/*
+#include <stdio.h>
+
+int main (void)
+{
+	int a = 45;
+	int b = 5;
+	int i = 0;
+	int *tab = ft_range(a, b);
+
+	while (tab[i])
+	{
+		printf("%d ", tab[i]);
+		i++;
+	}
+	return (0);
+}*/
